4be8a42c29daf01dd632106bb7ad9270bd771385
komarov <fransiscodiaz81@gmail.com>
2021-08-31 22:39:56 +0700

    UI Fix

--- app/Views/halaman_admin/_header.php
+++ app/Views/halaman_admin/_header.php
@@ -66,7 +66,8 @@
                         <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
                                 <div class="user-nav d-sm-flex d-none"><span class="user-name text-bold-600"><?= session()->get('namalengkap_akunPengguna') ?></span><span class="user-status"><?= session()->get('level_akunPengguna') ?></span></div><span><img class="round" src="https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png" alt="avatar" height="40" width="40"></span>
                             </a>
-                            <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#"><i class="feather icon-user"></i> Edit Profile</a>
+                            <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#">
+                                <!-- <i class="feather icon-user"></i> Edit Profile</a> -->
                                 <div class="dropdown-divider"></div><a class="dropdown-item" href="<?= base_url('Credentials/keluar') ?>"><i class="feather icon-power"></i> Logout</a>
                             </div>
                         </li>
